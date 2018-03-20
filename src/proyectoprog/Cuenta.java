ab6f5536e7deca49d2f435ea07a6e848050509be
JoshuaDDominguez <32340127+JoshuaDDominguez@users.noreply.github.com>
2018-03-20 20:06:13 +0100

    Merge origin/master
    Conflicts:
    	src/display/Menu.form
    	src/display/Menu.java
    	src/ficheros/TratarCarta.java
    	src/proyectoprog/ProyectoProg.java

--- src/proyectoprog/Cuenta.java
+++ src/proyectoprog/Cuenta.java
@@ -24,122 +24,126 @@ public class Cuenta {
         Double precio = (double) 0;
         String producto = "";
         do {
-            producto = "";
-            producto = JOptionPane.showInputDialog("Elija producto");
-            switch (producto) {
-                case "Kebab":
-                    precio = Cuenta.this.sumarCuenta(producto);
-                    break;
+            try {
+                producto = "";
+                producto = JOptionPane.showInputDialog("Elija producto");
+                switch (producto) {
+                    case "Kebab":
+                        precio = Cuenta.this.sumarCuenta(producto);
+                        break;
 
-                case "Durum":
-                    precio = Cuenta.this.sumarCuenta(producto);
-                    break;
+                    case "Durum":
+                        precio = Cuenta.this.sumarCuenta(producto);
+                        break;
 
-                case "Lahmacun":
-                    precio = Cuenta.this.sumarCuenta(producto);
-                    break;
+                    case "Lahmacun":
+                        precio = Cuenta.this.sumarCuenta(producto);
+                        break;
 
-                case "Falafel":
-                    precio = Cuenta.this.sumarCuenta(producto);
-                    break;
+                    case "Falafel":
+                        precio = Cuenta.this.sumarCuenta(producto);
+                        break;
 
-                case "Arroz":
-                    precio = Cuenta.this.sumarCuenta(producto);
-                    break;
+                    case "Arroz":
+                        precio = Cuenta.this.sumarCuenta(producto);
+                        break;
 
-                case "Snacks":
-                    precio = Cuenta.this.sumarCuenta(producto);
-                    break;
+                    case "Snacks":
+                        precio = Cuenta.this.sumarCuenta(producto);
+                        break;
 
-                case "Nuggets":
-                    precio = Cuenta.this.sumarCuenta(producto);
-                    break;
+                    case "Nuggets":
+                        precio = Cuenta.this.sumarCuenta(producto);
+                        break;
 
-                case "7UP":
-                    precio = Cuenta.this.sumarCuenta(producto);
-                    break;
+                    case "7UP":
+                        precio = Cuenta.this.sumarCuenta(producto);
+                        break;
 
-                case "Nestea":
-                    precio = Cuenta.this.sumarCuenta(producto);
-                    break;
+                    case "Nestea":
+                        precio = Cuenta.this.sumarCuenta(producto);
+                        break;
 
-                case "Agua":
-                    precio = Cuenta.this.sumarCuenta(producto);
-                    break;
+                    case "Agua":
+                        precio = Cuenta.this.sumarCuenta(producto);
+                        break;
 
-                case "Cerveza":
-                    precio = Cuenta.this.sumarCuenta(producto);
-                    break;
+                    case "Cerveza":
+                        precio = Cuenta.this.sumarCuenta(producto);
+                        break;
 
-                case "Minipizza":
-                    precio = Cuenta.this.sumarCuenta(producto);
-                    break;
+                    case "Minipizza":
+                        precio = Cuenta.this.sumarCuenta(producto);
+                        break;
 
-                case "Pizza":
-                    precio = Cuenta.this.sumarCuenta(producto);
-                    break;
+                    case "Pizza":
+                        precio = Cuenta.this.sumarCuenta(producto);
+                        break;
 
-                case "Pizzote":
-                    precio = Cuenta.this.sumarCuenta(producto);
-                    break;
+                    case "Pizzote":
+                        precio = Cuenta.this.sumarCuenta(producto);
+                        break;
 
-                case "Hamburguesa":
-                    precio = Cuenta.this.sumarCuenta(producto);
-                    break;
+                    case "Hamburguesa":
+                        precio = Cuenta.this.sumarCuenta(producto);
+                        break;
 
-                case "Pepito":
-                    precio = Cuenta.this.sumarCuenta(producto);
-                    break;
+                    case "Pepito":
+                        precio = Cuenta.this.sumarCuenta(producto);
+                        break;
 
-                case "Perrito":
-                    precio = Cuenta.this.sumarCuenta(producto);
-                    break;
+                    case "Perrito":
+                        precio = Cuenta.this.sumarCuenta(producto);
+                        break;
 
-                case "Bocadillo":
-                    precio = Cuenta.this.sumarCuenta(producto);
-                    break;
+                    case "Bocadillo":
+                        precio = Cuenta.this.sumarCuenta(producto);
+                        break;
 
-                case "Sandwitch":
-                    precio = Cuenta.this.sumarCuenta(producto);
-                    break;
+                    case "Sandwitch":
+                        precio = Cuenta.this.sumarCuenta(producto);
+                        break;
 
-                case "Patatas":
-                    precio = Cuenta.this.sumarCuenta(producto);
-                    break;
+                    case "Patatas":
+                        precio = Cuenta.this.sumarCuenta(producto);
+                        break;
 
-                case "Alitas":
-                    precio = Cuenta.this.sumarCuenta(producto);
-                    break;
+                    case "Alitas":
+                        precio = Cuenta.this.sumarCuenta(producto);
+                        break;
 
-                case "Ensalada":
-                    precio = Cuenta.this.sumarCuenta(producto);
-                    break;
+                    case "Ensalada":
+                        precio = Cuenta.this.sumarCuenta(producto);
+                        break;
 
-                case "Kas":
-                    precio = Cuenta.this.sumarCuenta(producto);
-                    break;
+                    case "Kas":
+                        precio = Cuenta.this.sumarCuenta(producto);
+                        break;
 
-                case "Aquarius":
-                    precio = Cuenta.this.sumarCuenta(producto);
-                    break;
+                    case "Aquarius":
+                        precio = Cuenta.this.sumarCuenta(producto);
+                        break;
 
-                case "Trina":
-                    precio = Cuenta.this.sumarCuenta(producto);
-                    break;
+                    case "Trina":
+                        precio = Cuenta.this.sumarCuenta(producto);
+                        break;
 
-                case "Monster":
-                    precio = Cuenta.this.sumarCuenta(producto);
-                    break;
+                    case "Monster":
+                        precio = Cuenta.this.sumarCuenta(producto);
+                        break;
 
-                case "Sidra":
-                    precio = Cuenta.this.sumarCuenta(producto);
-                    break;
+                    case "Sidra":
+                        precio = Cuenta.this.sumarCuenta(producto);
+                        break;
 
-                default:
-                    producto = "";
-                    break;
+                    default:
+                        producto = "";
+                        break;
+                }
+            } catch (NullPointerException ex) {
+                producto = "";
             }
-        } while (producto != "");
+        } while (producto.equals(""));
 
         return precio;
     }
