9fc21bbe6f3dedfa16cbf9c50283a75e37e535b9
Gonzalo <Gonzalo@192.168.0.19>
2018-03-20 18:19:04 +0100

    Imagen

--- src/proyectoprog/ProyectoProg.java
+++ src/proyectoprog/ProyectoProg.java
@@ -1,4 +1,4 @@
-/*
+ /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -24,7 +24,8 @@ public class ProyectoProg {
         String calle = dir.selCalle();
         String local = dir.selRest(calle);
         carta.cargarCarta(local);
-        
+        Window ventana = new Window();
+        ventana.setVisible(true);
     }
 
 }
