e82f91de18ad2878e040d0108be6c2853c57cda3
ggarciaalvarez <ggarciaalvarez@victoria26.danielcastelao.org>
2018-03-21 10:20:55 +0100

    Error solucionado

---
+++ ProyectoProg-master/src/proyectoprog/ProyectoProg.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectoprog;
+
+import ficheros.Carta;
+import ficheros.TratarCarta;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Gonzalo
+ */
+public class ProyectoProg {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+        Direcion dir = new Direcion();
+        TratarCarta carta = new TratarCarta();
+        Window ventana = new Window();
+        Cuenta cuenta = new Cuenta();
+        ArrayList<Carta> cartas = new ArrayList<>();
+
+        String calle = dir.selCalle();
+        String local = dir.selRest(calle);
+        carta.cargarCarta(local);
+        System.out.println(cuenta.cuentaTotal());
+
+        ventana.setVisible(true);
+
+    }
+
+}
